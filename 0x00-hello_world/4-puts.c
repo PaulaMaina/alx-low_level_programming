418f3e5d9434891578b2ead0d86510091d4a346d
PaulaMaina <kabura.mainap@gmail.com>
2023-10-05 04:25:53 -0700

    Commenting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/* 
- * The puts() function prints a sentence about programming
+/** 
+ * Description: The main function contains a puts function
+ * that prints a sentence about programming
+ * Return: 0 if there are no errors
  */
-
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
