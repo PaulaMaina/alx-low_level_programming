3556eb727f49631ff90a74ecae95321c71593a2e
PaulaMaina <kabura.mainap@gmail.com>
2023-10-05 04:51:43 -0700

    First commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/* Description: The program is about coding grammar */
+#include <stdio.h>
+/**
+ * main- prints a statement using printf
+ * Return: 0 if there are no errors
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
