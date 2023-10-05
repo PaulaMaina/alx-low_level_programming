9fcccaa1bbee21d0171a429752ae39d3b76dfc45
PaulaMaina <kabura.mainap@gmail.com>
2023-10-05 04:32:17 -0700

    Short description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+/*
+ * Description: The program prints a sentence about programming
+ */
+int main(void)
 /** 
  * Description: The main function contains a puts function
  * that prints a sentence about programming
  * Return: 0 if there are no errors
  */
-int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
