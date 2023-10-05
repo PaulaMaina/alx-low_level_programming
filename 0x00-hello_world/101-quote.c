1ff83c18ff41b803903eaee271183611704b79ca
PaulaMaina <kabura.mainap@gmail.com>
2023-10-05 05:53:29 -0700

    Debgging quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 /* The program prints a quote */
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main-prints a quote
  * Return:1
  */
 int main(void)
 {
-	fputs(stdout,
-			" and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			59);
 	return (1);
 }
