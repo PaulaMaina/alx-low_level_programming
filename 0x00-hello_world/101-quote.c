ea8e5813db55e314426037bf4cff54cae42f97a7
PaulaMaina <kabura.mainap@gmail.com>
2023-10-05 05:47:13 -0700

    First commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+/* The program prints a quote */
+#include <stdio.h>
+/**
+ * main-prints a quote
+ * Return:1
+ */
+int main(void)
+{
+	fputs(stdout,
+			" and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
