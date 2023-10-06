b975282502fb14df7cd8c45d5f1fe6a3e9e9b980
PaulaMaina <kabura.mainap@gmail.com>
2023-10-06 03:41:51 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+/*
+ * Author: Pauline Maina
+ * Description: This program prints all possible combinations of single numbers
+ */
+#include <stdio.h>
+
+/**
+ * main-entry point of the program
+ * Return: Always 0 is not errors are encountered
+ */
+int main(void)
+{
+	int n;
+	int number;
+
+	number = n % 10;
+	for (number = 0; number <= 9; number++)
+	{
+		putchar(48 + number);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
