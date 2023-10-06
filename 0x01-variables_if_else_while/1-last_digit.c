942a467460d5be52b1a72333f5387a58a4cf9714
PaulaMaina <kabura.mainap@gmail.com>
2023-10-06 02:20:54 -0700

    Task 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+/*
+ * Author: Pauline Maina
+ * Description: This program prints the last digit of a number
+ */
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main-entry point of the program
+ * Return: Always 0 is not errors are encountered
+ */
+int main(void)
+{
+	int n;
+	int lastDigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastDigit = n % 10;
+	if (lastDigit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+	}
+	else if (lastDigit == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+	}
+	return (0);
+}
