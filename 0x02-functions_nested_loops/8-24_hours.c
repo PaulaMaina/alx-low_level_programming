6220844b133d5bc9d3827f75b12a344d2b75e6da
PaulaMaina <kabura.mainap@gmail.com>
2023-10-11 03:54:50 -0700

    Debug task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,20 +5,20 @@
  */
 void jack_bauer(void)
 {
-	int hr_digit1, hr_digit2;
+	int h_digit1, hr_digit2;
 	int min_digit1, min_digit2;
 
-	for (hr_digit1 = 0; hr_digit1 <= 2; hr_digit1++)
+	for (h_digit1 = 0; h_digit1 <= 2; h_digit1++)
 	{
 		for (hr_digit2 = 0; hr_digit2 <= 9; hr_digit2++)
 		{
-			if ((hr_digit1 <= 1 && hr_digit2 <= 9) || (hr_digit1 <= 2 && hr_digit2 <= 3))
+			if ((h_digit1 <= 1 && hr_digit2 <= 9) || (h_digit1 <= 2 && hr_digit2 <= 3))
 			{
 				for (min_digit1 = 0; min_digit1 <= 5; min_digit1++)
 				{
 					for (min_digit2 = 0; min_digit2 <= 9; min_digit2++)
 					{
-						_putchar(48 + hr_digit1);
+						_putchar(48 + h_digit1);
 						_putchar(48 + hr_digit2);
 						_putchar(58);
 						_putchar(48 + min_digit1);
