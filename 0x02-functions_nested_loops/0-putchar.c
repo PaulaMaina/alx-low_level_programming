942caec91414874605e3e3a2e1f3bb9de006b08e
PaulaMaina <kabura.mainap@gmail.com>
2023-10-11 03:22:52 -0700

    Debug task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,14 +9,14 @@
  */
 int main(void)
 {
-	char word[8] = "_putchar";
-	int i = 0;
-
-	while (i < strlen(word))
-	{
-		_putchar(word[i]);
-		i++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
