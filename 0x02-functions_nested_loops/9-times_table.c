b445521534bf7e9ae74ac5d35471aa8227f53489
PaulaMaina <kabura.mainap@gmail.com>
2023-10-11 02:42:22 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,50 @@
+#include "math.h"
+/**
+ * main - invokes the times_table function
+ * Return: Always 0
+ */
+void times_table(void);
+int main(void)
+{
+	times_table();
+	return (0);
+}
+/**
+ * times_table - prints the times table of 9
+ * Return: void
+ */
+void times_table(void)
+{
+	int number, multiplier, result;
+	int i, j;
+
+	for (number = 0; number <= 9; number++)
+	{
+		for (multiplier = 0; multiplier <= 9; multiplier++)
+		{
+			result = number * multiplier;
+
+			if (result > 9)
+			{
+				i = result % 10;
+				j = (result - i) / 10;
+
+				_putchar(',');
+				_putchar(' ');
+				_putchar(48 + j);
+				_putchar(48 + i);
+			}
+			else
+			{
+				if (multiplier != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(48 + result);
+			}
+		}
+		_putchar('\n');
+	}
+}
