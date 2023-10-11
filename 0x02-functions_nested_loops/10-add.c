5cf7413d2de5c4dc1460731222c8aa32fb1af5c2
PaulaMaina <kabura.mainap@gmail.com>
2023-10-11 02:46:04 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ * add - add two integers
+ * @int: data type of the parameters
+ * Return: result of add
+ */
+int add(int, int)
+{
+	int num1, num2;
+	int total;
+
+	total = num1 + num2;
+
+	return (total);
+}
