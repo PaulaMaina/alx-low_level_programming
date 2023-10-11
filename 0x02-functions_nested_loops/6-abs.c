51cbd9578326038f0da9ca4ac07ce64b962efeb8
PaulaMaina <kabura.mainap@gmail.com>
2023-10-11 01:35:00 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+/**
+ * _abs - Computes the absolute value of a number
+ * @int: data type of the parameter
+ * Return: The absolute value
+ */
+int _abs(int)
+{
+	int number;
+
+	if (number < 0)
+	{
+		int abs_value;
+
+		abs_value = number * -1;
+
+		return (abs_value);
+	}
+	else
+	{
+		return (number);
+	}
+}
