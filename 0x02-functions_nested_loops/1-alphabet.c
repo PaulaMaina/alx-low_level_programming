20cc5d52526259ae1be6c8c48a0a0b9494f7b0a8
PaulaMaina <kabura.mainap@gmail.com>
2023-10-11 03:26:05 -0700

    Debug task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,18 +3,6 @@
  */
 #include "main.h"
 /**
- * main-invokes the print_alphabet function
- * Return: 0 if no errors are encountered
- */
-void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
  * print_alphabet-prints the letters of the alphabet in lowercase
  * return: void
  */
