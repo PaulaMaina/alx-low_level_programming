f38e4d6c3b946d19fe0b4ad7fd86d9172a53b3f8
PaulaMaina <kabura.mainap@gmail.com>
2023-10-11 03:27:24 -0700

    Debug task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,17 +3,6 @@
  */
 #include "main.h"
 /**
- * main-invokes the print_aplhabet function 10 times
- * Return: Always 0 if no errors are encountered
- */
-void print_alphabet_x10(void);
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
-
-/**
  * print_alphabet_x10-prints the alphabet in lowercase 10 times
  * Return: void
  */
