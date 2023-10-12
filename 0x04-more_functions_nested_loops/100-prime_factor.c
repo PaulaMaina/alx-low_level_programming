1fc5c197f3508d02bf217c345f7438d886f74ce7
PaulaMaina <kabura.mainap@gmail.com>
2023-10-12 04:05:38 -0700

    Task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - prints the prime factors of a 612852475143
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 612852475143;
+	long a, maxFactor;
+	double sq = sqrt(num);
+
+	for (a = 1; x <= sq; a++)
+	{
+		if (num % a == 0)
+			maxFactor = num / a;
+	}
+	printf("%ld", maxFactor);
+	return (0);
+}
