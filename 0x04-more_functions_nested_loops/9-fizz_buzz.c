9795b8d60428f8487532cd9a53918351278e6c0f
PaulaMaina <kabura.mainap@gmail.com>
2023-10-12 03:07:21 -0700

    Space after 100

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,24 +11,23 @@ int main(void)
 	{
 		if (n % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (n % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if ((n % 3 == 0) && (n % 5 == 0))
 		{
-			printf("FizzBuzz ");
-		}
-		else if (n == 100)
-		{
-			print("Buzz");
+			printf("FizzBuzz");
 		}
 		else
 		{
-			printf("%d ", n);
+			printf("%d", n);
 		}
+
+		if (n != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
