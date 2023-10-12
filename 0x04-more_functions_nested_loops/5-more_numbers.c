04a2f2d5d7386c0f0bc30a278f63f77d8a667d24
PaulaMaina <kabura.mainap@gmail.com>
2023-10-12 02:22:23 -0700

    Debug task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * more_numbers - prins the numbers 0 to 14 ten times
+ * more_numbers - prints the numbers 0 to 14 ten times
  * Return: void
  */
 void more_numbers(void)
@@ -11,8 +11,10 @@ void more_numbers(void)
 	{
 		for (b = 0; b <= 14; b++)
 		{
-			if (b > 0)
+			if (b > 9)
+			{
 				_putchar(48 + (b / 10));
+			}
 
 			_putchar(48 + (y % 10));
 		}
