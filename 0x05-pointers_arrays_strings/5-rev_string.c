32589aaa1c6e94efc459a6ba076b1b808c0f056a
PaulaMaina <kabura.mainap@gmail.com>
2023-10-16 01:29:32 -0700

    First commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int elem = 0;
+	int index = 0;
+	char *rev_str;
+
+	while (s[elem] != '\0')
+	{
+		elem++;
+	}
+
+	elem -= 1;
+
+	while (s[elem] != '\0')
+	{
+		rev_str[index] = s[elem];
+		index++;
+		elem--;
+	}
+	rev_str[index + 1] = '\0';
+}
