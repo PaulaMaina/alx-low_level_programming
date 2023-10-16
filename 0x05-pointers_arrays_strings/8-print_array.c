1772bd29e68293c44bb1038dba1ab1988fe2274e
PaulaMaina <kabura.mainap@gmail.com>
2023-10-16 02:33:12 -0700

    First commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_array - prints elements of an array of integers
+ * @a: the array
+ * @n: the length of the array
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+		if (a[index] == '\0')
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf("%d, ", a[index]);
+		}
+	}
+}
