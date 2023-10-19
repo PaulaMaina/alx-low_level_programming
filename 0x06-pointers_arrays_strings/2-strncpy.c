5b36113589a63fda8580a4813f075118b6b600cc
PaulaMaina <kabura.mainap@gmail.com>
2023-10-19 02:48:03 -0700

    First commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @src: source string
+ * @dest: destination string
+ * @n: number of charactesrs to be copied
+ *
+ * Return: pointer to the resulting string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	for (a = 0; a < n && src[a] != '\0'; a++)
+		dest[a] = src[a];
+
+	for (a = 0; n > a; a++)
+		dest[a] = '\0';
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: negative integer if s1 < s2, 0 if s1 == s2,
+ * and positive if s2 > s1
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int a;
+	int result;
+
+	for (a = 0; s1[a] != '\0' || s2[a] != '\0'; a++)
+	{
+		if (s1[a] != s2[a])
+		{
+			result = s1[a] - s2[a];
+			return (result);
+		}
+
+		return (0);
+	}
+}
