b9bdd2e0d974144105bce48372e12c1debd1811e
PaulaMaina <kabura.mainap@gmail.com>
2023-10-19 05:27:40 -0700

    First commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: string
+ *
+ * Return: str
+ */
+char *leet(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != 0; a++)
+	{
+		while (str[a] == 'a' || str[a] == 'A')
+			str[a] = 4;
+		while (str[a] == 'e' || str[a] == 'E')
+			str[a] == 3;
+		while (str[a] == 'o' || str[a] == 'O')
+			str[a] == 0;
+		while (str[a] == 't' || str[a] == 'T')
+			str[a] == 7;
+		while (str[a] == 'l' || str[a] == 'L')
+			str[a] = 1;
+	}
+
+	return (str);
+}
