a06744e936318e118dbae6a90df68021407c2ce3
PaulaMaina <kabura.mainap@gmail.com>
2023-10-19 05:43:19 -0700

    characters not numbers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,15 +13,15 @@ char *leet(char *str)
 	for (a = 0; str[a] != '\0'; a++)
 	{
 		while (str[a] == 'a' || str[a] == 'A')
-			str[a] = 4;
+			str[a] = '4';
 		while (str[a] == 'e' || str[a] == 'E')
-			str[a] == 3;
+			str[a] == '3';
 		while (str[a] == 'o' || str[a] == 'O')
-			str[a] == 0;
+			str[a] == '0';
 		while (str[a] == 't' || str[a] == 'T')
-			str[a] == 7;
+			str[a] == '7';
 		while (str[a] == 'l' || str[a] == 'L')
-			str[a] = 1;
+			str[a] = '1';
 	}
 
 	return (str);
