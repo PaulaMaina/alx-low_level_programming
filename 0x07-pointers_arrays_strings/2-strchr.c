ae619dab77000cb9b3a25139825e974129d64023
PaulaMaina <kabura.mainap@gmail.com>
2023-10-23 05:24:55 -0700

    First commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to be searched
+ * @c: character to be located
+ *
+ * Return: pointer to the first occurrence of c in s
+ * or NULL if the character is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (s[a] == c)
+			return (s[a]);
+		else
+			return (NULL);
+	}
+}
