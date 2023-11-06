815f29e6292787b6a819f4aef0fba45e27f0e8ca
PaulaMaina <kabura.mainap@gmail.com>
2023-11-06 10:14:13 -0800

    new-dog debug

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,16 +18,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *cp_name, *cp_owner;
 
 	dog_new = malloc(sizeof(struct dog));
-
 	if (dog_new == NULL)
 		return (NULL);
+
 	for (a = 0; name[a] != '\0'; a++)
 		;
 	for (b = 0; owner[b] != '\0'; b++)
 		;
 
 	cp_name = malloc(sizeof(char) * a + 1);
-
 	if (cp_name == NULL)
 	{
 		free(dog_new);
@@ -35,21 +34,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 
 	cp_owner = malloc(sizeof(char) * b + 1);
-
 	if (cp_owner == NULL)
 	{
 		free(cp_name);
 		free(dog_new);
 		return (NULL);
 	}
+
 	for (c = 0; c < a; c++)
 		cp_name[c] = name[c];
 	for (c = 0; c < b; c++)
 		cp_owner[c] = owner[c];
 
-	dog->name = cp_name;
-	dog->age = age;
-	dog->owner = cp_owner;
+	dog_new->name = cp_name;
+	dog_new->age = age;
+	dog_new->owner = cp_owner;
 
 	return (dog_new);
 }
