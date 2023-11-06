912434f6cc59a149e17bb890be6ab4f6e62672ae
PaulaMaina <kabura.mainap@gmail.com>
2023-11-06 08:20:20 -0800

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - new type struct dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
