0afa1a382258d043cc425172b44c004630ee4085
PaulaMaina <kabura.mainap@gmail.com>
2023-11-08 05:01:16 -0800

    Task 0 & 1 debug

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,8 +4,6 @@
  * print_name - prints a name
  * @name: name to be printed
  * @f: pointer to a function
- *
- * Return: nothing
  */
 
 void print_name(char *name, void (*f)(char *))
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,8 +6,6 @@
  * @array: array
  * @size: size of the array
  * @action: pointer to the function to be used
- *
- * Return: nothing
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
