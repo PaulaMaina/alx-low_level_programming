1b7f308c2b49050100b72394a4afdfb4e49600e0
PaulaMaina <kabura.mainap@gmail.com>
2023-11-27 10:25:40 -0800

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -87,7 +87,7 @@ int main(int argc, char *argv[])
 	if (fd_target == -1)
 	{
 		_close(fd_src);
-		printf(STDERR_FILENO, "Error: Can't  write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't  write to %s\n", argv[2]);
 		exit(99);
 	}
 	cp_file(fd_src, fd_target, argv[2]);
