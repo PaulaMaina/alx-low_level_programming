5a2654b7981a57f4c57b60e6a480bf47ee778bf2
PaulaMaina <kabura.mainap@gmail.com>
2023-11-27 10:48:29 -0800

    Debug

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -93,7 +93,7 @@ int main(int argc, char *argv[])
 	if (fd_target == -1)
 	{
 		_close(fd_src);
-		dprintf(STDERR_FILENO, "Error: Can't  write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		return (99);
 	}
 	cp_file(fd_src, fd_target, argv[2]);
