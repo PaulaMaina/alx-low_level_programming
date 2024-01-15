e625c489f935787984b4620633f1788c4b41e7bd
PaulaMaina <kabura.mainap@gmail.com>
2024-01-15 06:54:27 -0800

    Operations

---
+++ 0x18-dynamic_libraries/simple-functions.c
@@ -0,0 +1,76 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result of addition
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
+
+/**
+ * sub - subtracts two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result of subtraction
+ */
+int sub(int a, int b)
+{
+	int result;
+
+	result = a - b;
+
+	return (result);
+}
+
+/**
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
+
+/**
+ * div - divides two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result of division
+ */
+int div(int a, int b)
+{
+	int result;
+
+	result = a / b;
+
+	return (result);
+}
+
+/**
+ * mod - returns the remainder of dividing two numbers
+ * @a: first number
+ * @b: second number
+ * Return: remainder
+ */
+int mod(int a, int b)
+{
+	int result;
+
+	result = a % b;
+
+	return (result);
+}
