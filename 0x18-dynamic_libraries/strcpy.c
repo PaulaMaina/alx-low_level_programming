7c5b42ca27383d674ad51947a32338ce9bb51e8f
PaulaMaina <kabura.mainap@gmail.com>
2024-01-15 06:25:38 -0800

    Functions

--- 0x18-dynamic_libraries/strcpy.c
+++ 0x18-dynamic_libraries/strcpy.c
@@ -1,23 +1,24 @@
 #include "main.h"
 
 /**
- * _strncpy - copies a string
- * @src: source string
- * @dest: destination string
- * @n: number of charactesrs to be copied
+ * _strcpy - copies the string pointer to by src,
+ * to the buffer pointed to by dest
+ * including \0
+ * @src: pointer to string to be copied
+ * @dest: location of copied string
  *
- * Return: pointer to the resulting string
+ * Return: the pointer to dest
  */
 
-char *_strncpy(char *dest, char *src, int n)
+char *_strcpy(char *dest, char *src)
 {
 	int a;
 
-	for (a = 0; a < n && src[a] != '\0'; a++)
+	for (a = 0; src[a] != '\0'; a++)
+	{
 		dest[a] = src[a];
-
-	for (; n > a; a++)
-		dest[a] = '\0';
+	}
+	dest[a] = '\0';
 
 	return (dest);
 }
