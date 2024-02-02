0926c4e6c6f87f4314724e3e028ddba243c0baf5
PaulaMaina <kabura.mainap@gmail.com>
2024-02-02 10:29:11 +0300

    Task 0 debug

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
 
-	if (size == 0)
+	if (size <= 0)
 		return (NULL);
 
 	hash_table = malloc(sizeof(*hash_table));
